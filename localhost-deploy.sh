b595c746022839216905656f38328dde7a6bc66a
spacemandev <devbharel@gmail.com>
2023-04-20 11:43:42 -0500

    fixed localhost deploy.sh

--- localhost-deploy.sh
+++ localhost-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-solana program deploy --program-id ./localkeys/core-devnet.json ./target/deploy/core_ds.so
-solana program deploy --program-id ./localkeys/registry-devnet.json ./target/deploy/registry.so
-solana program deploy --program-id ./localkeys/kyogen-devnet.json ./target/deploy/kyogen.so
-solana program deploy --program-id ./localkeys/structures-devnet.json ./target/deploy/structures.so
+solana program deploy --program-id ./localkeys/core-ds-keypair.json ./target/deploy/core_ds.so
+solana program deploy --program-id ./localkeys/registry-keypair.json ./target/deploy/registry.so
+solana program deploy --program-id ./localkeys/kyogen-keypair.json ./target/deploy/kyogen.so
+solana program deploy --program-id ./localkeys/structures-keypair.json ./target/deploy/structures.so
